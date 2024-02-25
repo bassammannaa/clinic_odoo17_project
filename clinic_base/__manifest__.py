6fdf9046cb78a73b879d0016dc013fab0e560e33
Bassam Mannaa <b_mannaa@hotmail.com>
2024-02-26 00:13:58 +0300

    lesson 4

--- clinic_base/__manifest__.py
+++ clinic_base/__manifest__.py
@@ -1,34 +1,35 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "clinic_base",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
+    'name': "Clinic Management System",
+    'summary': "Clinic Management System for managing Hospital and medical facilities flows.",
     'description': """
-Long description of module's purpose
+                A clinic management software or practice management software provides a suite of 
+                functionalities that makes it easy to manage a clinic. A medical practice management 
+                system is used to manage the patients, the appointments, the doctors' schedules, 
+                prescriptions, manage inventory, etc.
     """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
+    'author': "Bassam Mannaa",
+    'website': "https://odoo-professional.com",
+    'support': "b_mannaa@hotmail.com",
+    'category': 'Medical',
+    'license': 'OPL-1',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base'],
-
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
-    # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    'images': [
+        'static/description/icon.png'
+        ],
+    'installable': True,
+    'application': True,
+    'sequence': 1,
+    'price': 36,
+    'currency': 'USD',
 }
+
 
