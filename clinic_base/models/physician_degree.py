f85e63273339ce8b4f7379eb033d86ab278cadff
Bassam Mannaa <b_mannaa@hotmail.com>
2024-02-28 22:25:20 +0300

    Lesson 6

---
+++ clinic_base/models/physician_degree.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _, tools
+import logging
+from odoo.exceptions import ValidationError
+import datetime
+
+logger = logging.getLogger(__name__)
+
+
+class PhysicianDegree(models.Model):
+    _name = 'clinic.physician.degree'
+    _rec_name = 'name'
+    _description = 'Store Physician Degrees'
+
+    name = fields.Char()
+
+    @api.model_create_multi
+    def create(self, vals):
+        try:
+            obj = super(PhysicianDegree, self).create(vals)
+        except Exception as e:
+            logger.exception("create Method")
+            raise ValidationError(e)
+        return obj
+
+    def write(self, vals):
+        try:
+            obj = super(PhysicianDegree, self).write(vals)
+        except Exception as e:
+            logger.exception("Write Method")
+            raise ValidationError(e)
+        return obj
+
+    def unlink(self):
+        try:
+            return super(PhysicianDegree, self).unlink()
+        except Exception as e:
+            logger.exception("unlink Method")
+            raise ValidationError(e)
